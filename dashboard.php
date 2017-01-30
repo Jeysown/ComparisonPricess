58a681720edb49c4d06719f247ea468e59149ba4
Jeysown <jeysownlopez@gmail.com>
2017-01-30 16:56:30 +0800

    adsa

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="assets/fonts/css/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title">
   </head>
-  <body id="page-top" onload="createColumnChartoverview()">
+  <body id="page-top" onload="createColumnChartoverview();initialize();">
   <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
       <div class="container">
           <div class="navbar-header page-scroll">
@@ -44,7 +44,9 @@
 
   <!-- about -->
   <section id="Maps" style="height:700px;">
-       <iframe src="https://www.google.com/maps/embed/v1/place?q=Technological%20Institute%20of%20the%20Philippines&zoom=17 &key=AIzaSyBVPZIWcBs6SVr5uECLmrhe8c-9DD8dGe0" style="height:700px;width:100%;border:none"></iframe>
+      <form id="form1" runat="server">
+        <div id="map_canvas" style="height:700px;width:100%;border:none"></div>
+      </form>
 
   </section>
   <section id="Interpretation" style="background:#f1f1f1;height:580px;background-attachment:fixed;">
--- footer.php
+++ footer.php
@@ -22,7 +22,85 @@
 <script src="assets/js/charts.js"></script>
     <script src="assets/js/header.js"></script>
     <script src="assets/js/ease.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script type="text/javascript">
 
+
+    var markerOptions = {gridSize: 15, maxZoom: 18, imagePath:  'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'};
+
+    function initialize() {
+      var markerCluster;
+      var markerss =[];
+      var marker;
+      var mapOptions = {
+        center: new google.maps.LatLng(14.178609,121.1848123),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+      var infowindow = new google.maps.InfoWindow();
+
+      var getLabel = function(data){
+        var label = "A"
+        if(data=="lagunaprivateresort.com"){
+          label = "A";
+        }else if(data == "pansolresorts.com.ph"){
+          label = "A";
+        }else if(data == "lagunaspringresorts.com "){
+          label = "A";
+        }else if(data == "lagunaprivatepools.com"){
+          label = "A";
+        }else if(data == "calambaresorts.com.ph"){
+          label = "A";
+        }else if(data == "pansolresorts.ph"){
+          label = "A";
+        }else if(data == "lagunaresorts.com.ph"){
+          label = "A";
+        }
+        return label;
+      }
+
+
+      $.getJSON('data.json', function(data) {
+        $.each( data, function(i, value) {
+          var myLatlng = new google.maps.LatLng(value.lat, value.long);
+          var content = value.website +"\n"+value.resort+"\n"+value.address;
+
+          marker = new google.maps.Marker({
+            position: myLatlng,
+            //icon: getCustomIcons(value.website),
+            label: getLabel(value.website),
+            title: value.website,
+            map: map
+          });
+          markerss.push(marker);
+          bindInfoWindow(marker, map, infowindow, content);
+
+        });
+
+        markerCluster = new MarkerClusterer(map,markerss,markerOptions);
+
+      });
+
+    }
+
+
+    function bindInfoWindow(marker, map, infowindow, content) {
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
+      });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlbfgiXejCsBBpDWR3WedpCkdSNbS6ZXc&callback=initMap">
+    </script>
     <script type="text/javascript">
      $("#sidebar-content").load("listofresort.php");
      $('#Overview').click(function(){
